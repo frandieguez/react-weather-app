105a31ee62a5452076bca25432f8dbc05598e999
Fran Dieguez <fran.dieguez@mabishu.com>
2019-01-03 15:32:34 +0100

    Use the state to initializate the WeatherLocation component

--- weather-app/src/components/WeatherLocation/index.js
+++ weather-app/src/components/WeatherLocation/index.js
@@ -2,16 +2,40 @@ import React, { Component } from 'react';
 import Location from './Location';
 import WeatherData from './WeatherData';
 import './style.css';
+import { CLOUD, SUN } from '../../constants/weathers';
 
 class WeatherLocation extends Component {
 
+  constructor(props) {
+    super(props);
+    this.state = {
+      city: props.data.city,
+      data: {
+        temperature: props.data.temperature,
+        weatherState: props.data.weatherState,
+        humidity: props.data.humidity,
+        wind: props.data.wind,
+      }
+    }
+  }
+
+  handleUpdateClick = () => {
+    this.setState({
+      data: {
+        temperature: 20,
+        weatherState: SUN,
+      }
+    });
+  }
+
   render () {
-    let { data } = this.props;
+    let { city, data } = this.state;
 
     return (
       <div className="weatherLocationContainer">
-          <Location city={data.city}></Location>
+          <Location city={city}></Location>
           <WeatherData data={data}></WeatherData>
+          <button onClick={this.handleUpdateClick}>Update</button>
       </div>
     );
   }
