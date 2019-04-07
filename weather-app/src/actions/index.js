6cc0c6a5fa833bd40cb741d64a7a0d05cb50ea93
Fran Dieguez <fran.dieguez@mabishu.com>
2019-04-07 12:44:03 +0200

    Port ForecastExtended::fetchDat method to redux action setSelectedCity

--- weather-app/src/actions/index.js
+++ weather-app/src/actions/index.js
@@ -1,4 +1,30 @@
+import * as Openweather from '../services/api_url';
+import { transformForecast} from '../services/transformWeather';
+
 export const SET_CITY = 'SET_CITY'
+export const SET_FORECAST_DATA = 'SET_FORECAST_DATA'
 
 // Create an action creator
-export const setCity = payload => ({ type: SET_CITY, payload });
+const setCity = payload => ({ type: SET_CITY, payload });
+const setForecastData = payload => ({ type: SET_FORECAST_DATA, payload});
+
+export const setSelectedCity = payload => {
+  return dispatch => {
+    dispatch(setCity(payload));
+
+    // activate in state an data searched indicator
+    fetch(Openweather.getForecastUrl(payload)).then((data) => {
+      return data.json();
+    }).then( weatherData => {
+      console.log(payload)
+      if (!weatherData.list) {
+        return;
+      }
+
+      var forecastData = transformForecast(weatherData);
+
+      // change state with the Promise result (fetch)
+      dispatch(setForecastData({ city: payload, forecastData }))
+    })
+  }
+}
