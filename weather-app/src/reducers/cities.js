0efeb9315c920e1c51e732f94b581527512229af
Fran Dieguez <fran.dieguez@mabishu.com>
2019-04-07 12:44:41 +0200

    Use combineReducers and split reducers

--- weather-app/src/reducers/cities.js
+++ weather-app/src/reducers/cities.js
@@ -1,13 +1,15 @@
-import { SET_CITY } from '../actions'
+import { SET_FORECAST_DATA } from '../actions'
 
 // In order to city be a pure function it must:
 // - Dont alter the state with state.city = VALUE
 // - Create a copy of the state with {...state, CHANGES_HERE }
-export const city = (state = {}, action) => {
+export const cities = (state = {}, action) => {
 
   switch (action.type) {
-    case SET_CITY:
-      return action.payload
+    case SET_FORECAST_DATA:
+      const { city, forecastData } = action.payload;
+
+      return { ...state, [city]: { forecastData } }
     default:
       break;
   }
