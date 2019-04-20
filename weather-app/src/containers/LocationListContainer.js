154c340a04d59e08ed013f7580863d78369eaa36
Fran Dieguez <fran.dieguez@mabishu.com>
2019-04-20 17:59:36 +0200

    Add commenta about how to use bindActionCreators function from redus in LocationListcontainer

--- weather-app/src/containers/LocationListContainer.js
+++ weather-app/src/containers/LocationListContainer.js
@@ -1,6 +1,7 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import { bindActionCreators } from 'redux';
 import { connect } from 'react-redux';
-import { setSelectedCity, setWeather } from '../actions'
+import * as actions from '../actions'
 import { getWeatherCities, getCity } from '../reducers'
 import PropTypes from 'prop-types';
 import LocationList from '../components/LocationList';
@@ -36,9 +37,12 @@ const mapStateToProps = state => ({
   city: getCity(state)
 })
 
+// The two assignements below are equivalent, redux provides a helper to
+// automatically match actions to state.
+// const mapDispatchToProps = dispatch => bindActionCreators(actions, dispatch);
 const mapDispatchToProps = dispatch => ({
-  setSelectedCity: value => dispatch(setSelectedCity(value)),
-  setWeather: cities => dispatch(setWeather(cities))
+  setSelectedCity: value => dispatch(actions.setSelectedCity(value)),
+  setWeather: cities => dispatch(actions.setWeather(cities))
 });
 
 export default connect(mapStateToProps, mapDispatchToProps)(LocationListContainer);
